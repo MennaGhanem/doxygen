da120f63802b0b1391a0d9252d3f7948cab85720
MENNA <menna1301@gmail.com>
2022-03-23 10:42:58 +0200

    add doxygen_out file

---
+++ Menna-tallah Gamal/doxygen_out/html/search/all_1.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['main_2ec',['main.c',['../main_8c.html',1,'']]],
+  ['motor_2ec',['motor.c',['../motor_8c.html',1,'']]],
+  ['motor_2eh',['motor.h',['../motor_8h.html',1,'']]],
+  ['motor_5fleft',['motor_left',['../motor_8c.html#a4281d4ee0f4f2ec50152319d400310a1',1,'motor_left(uint8 PIN1, uint8 PIN2):&#160;motor.c'],['../motor_8h.html#a4281d4ee0f4f2ec50152319d400310a1',1,'motor_left(uint8 PIN1, uint8 PIN2):&#160;motor.c']]],
+  ['motor_5fright',['motor_right',['../motor_8c.html#a6b55a2cf8d7627f4a788939610935091',1,'motor_right(uint8 PIN1, uint8 PIN2):&#160;motor.c'],['../motor_8h.html#a6b55a2cf8d7627f4a788939610935091',1,'motor_right(uint8 PIN1, uint8 PIN2):&#160;motor.c']]],
+  ['motor_5fstop',['motor_stop',['../motor_8c.html#af56c7511697f620ca362c774e1afaf1a',1,'motor_stop(uint8 PIN1, uint8 PIN2):&#160;motor.c'],['../motor_8h.html#af56c7511697f620ca362c774e1afaf1a',1,'motor_stop(uint8 PIN1, uint8 PIN2):&#160;motor.c']]]
+];
---
+++ Menna-tallah Gamal/doxygen_out/html/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['motor_5fleft',['motor_left',['../motor_8c.html#a4281d4ee0f4f2ec50152319d400310a1',1,'motor_left(uint8 PIN1, uint8 PIN2):&#160;motor.c'],['../motor_8h.html#a4281d4ee0f4f2ec50152319d400310a1',1,'motor_left(uint8 PIN1, uint8 PIN2):&#160;motor.c']]],
+  ['motor_5fright',['motor_right',['../motor_8c.html#a6b55a2cf8d7627f4a788939610935091',1,'motor_right(uint8 PIN1, uint8 PIN2):&#160;motor.c'],['../motor_8h.html#a6b55a2cf8d7627f4a788939610935091',1,'motor_right(uint8 PIN1, uint8 PIN2):&#160;motor.c']]],
+  ['motor_5fstop',['motor_stop',['../motor_8c.html#af56c7511697f620ca362c774e1afaf1a',1,'motor_stop(uint8 PIN1, uint8 PIN2):&#160;motor.c'],['../motor_8h.html#af56c7511697f620ca362c774e1afaf1a',1,'motor_stop(uint8 PIN1, uint8 PIN2):&#160;motor.c']]]
+];
